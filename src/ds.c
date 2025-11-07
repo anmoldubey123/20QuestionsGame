85c15abc74e2ca378b6c6d9fbf07f14ea7698659
Anmol Dubey <ad56328@mario.ece.utexas.edu>
2025-11-07 04:33:48 -0600

    checkpoint before creating claude debug branch

--- src/ds.c
+++ src/ds.c
@@ -4,6 +4,7 @@
 #include <ctype.h>
 #include "lab5.h"
 
+
 /* ========== Node Functions ========== */
 
 /* TODO 1: Implement create_question_node
@@ -13,18 +14,40 @@
  * - Initialize yes and no pointers to NULL
  * - Return the new node
  */
-Node *create_question_node(const char *question) {
-    // TODO: Implement this function
-    return NULL;
+Node *create_question_node(const char *question) 
+{
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if(newNode==NULL)
+    {
+        return NULL;
+    }
+
+    newNode->text = strdup(question);
+    newNode->yes = NULL;
+    newNode->no = NULL;
+    newNode->isQuestion = 1;
+
+    return newNode;
 }
 
 /* TODO 2: Implement create_animal_node
  * - Similar to create_question_node but set isQuestion to 0
  * - This represents a leaf node with an animal name
  */
-Node *create_animal_node(const char *animal) {
-    // TODO: Implement this function
-    return NULL;
+Node *create_animal_node(const char *animal) 
+{
+    Node* animalNode = (Node*)malloc(sizeof(Node));
+    if(animalNode==NULL)
+    {
+        return NULL;
+    }
+
+    animalNode->text = strdup(animal);
+    animalNode->yes = NULL;
+    animalNode->no = NULL;
+    animalNode->isQuestion = 0;
+
+    return animalNode;
 }
 
 /* TODO 3: Implement free_tree (recursive)
@@ -36,17 +59,31 @@ Node *create_animal_node(const char *animal) {
  * - Free the node itself
  * IMPORTANT: Free children before freeing the parent!
  */
-void free_tree(Node *node) {
-    // TODO: Implement this function
+void free_tree(Node *node) 
+{
+    if(node==NULL)
+    {
+        return;
+    }
+    
+    free_tree(node->yes);
+    free_tree(node->no);
+    free(node->text);
+    free(node);
 }
 
 /* TODO 4: Implement count_nodes (recursive)
  * - Base case: if root is NULL, return 0
  * - Return 1 + count of left subtree + count of right subtree
  */
-int count_nodes(Node *root) {
-    // TODO: Implement this function
-    return 0;
+int count_nodes(Node *root) 
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+
+    return 1 + count_nodes(root->yes) + count_nodes(root->no);
 }
 
 /* ========== Frame Stack (for iterative tree traversal) ========== */
@@ -56,8 +93,18 @@ int count_nodes(Node *root) {
  * - Set size to 0
  * - Set capacity to 16
  */
-void fs_init(FrameStack *s) {
-    // TODO: Implement this function
+void fs_init(FrameStack *s) 
+{
+    s->frames = (Frame*)malloc(16*sizeof(Frame));
+    if(s->frames==NULL)
+    {
+        s->size = 0;
+        s->capacity = 0;
+        return;
+    }
+
+    s->size = 0;
+    s->capacity = 16;
 }
 
 /* TODO 6: Implement fs_push
@@ -66,8 +113,34 @@ void fs_init(FrameStack *s) {
  * - Store the node and answeredYes in frames[size]
  * - Increment size
  */
-void fs_push(FrameStack *s, Node *node, int answeredYes) {
-    // TODO: Implement this function
+void fs_push(FrameStack *s, Node *node, int answeredYes) 
+{
+    int newCapacity;
+    if(s->size >= s->capacity)
+    {
+        if(s->capacity==0)
+        {
+            newCapacity = 1;
+        }
+        else 
+        {
+            newCapacity = s->capacity * 2;
+        }
+        
+        Frame* temp = realloc(s->frames, newCapacity*sizeof(Frame));
+        if(temp==NULL)
+        {
+            return;
+        }
+
+        s->frames = temp;
+        s->capacity = newCapacity;
+    }
+
+    s->frames[s->size].node = node;
+    s->frames[s->size].answeredYes = answeredYes;
+
+    s->size = s->size + 1;
 }
 
 /* TODO 7: Implement fs_pop
@@ -75,18 +148,23 @@ void fs_push(FrameStack *s, Node *node, int answeredYes) {
  * - Return the frame at frames[size]
  * Note: No need to check if empty - caller should use fs_empty() first
  */
-Frame fs_pop(FrameStack *s) {
-    Frame dummy = {NULL, -1};
-    // TODO: Implement this function
-    return dummy;
+Frame fs_pop(FrameStack *s) 
+{
+    s->size = s->size - 1;
+    return s->frames[s->size];
 }
 
 /* TODO 8: Implement fs_empty
  * - Return 1 if size == 0, otherwise return 0
  */
-int fs_empty(FrameStack *s) {
-    // TODO: Implement this function
-    return 1;
+int fs_empty(FrameStack *s) 
+{
+    if(s->size==0)
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
 /* TODO 9: Implement fs_free
@@ -94,8 +172,12 @@ int fs_empty(FrameStack *s) {
  * - Set frames pointer to NULL
  * - Reset size and capacity to 0
  */
-void fs_free(FrameStack *s) {
-    // TODO: Implement this function
+void fs_free(FrameStack *s) 
+{
+    free(s->frames);
+    s->frames = NULL;
+    s->size = 0;
+    s->capacity = 0;
 }
 
 /* ========== Edit Stack (for undo/redo) ========== */
@@ -103,8 +185,18 @@ void fs_free(FrameStack *s) {
 /* TODO 10: Implement es_init
  * Similar to fs_init but for Edit structs
  */
-void es_init(EditStack *s) {
-    // TODO: Implement this function
+void es_init(EditStack *s) 
+{
+    s->edits = (Edit*)malloc(16*sizeof(Edit));
+    if(s->edits==NULL)
+    {
+        s->size = 0;
+        s->capacity = 0;
+        return;
+    }
+
+    s->size = 0;
+    s->capacity = 16;
 }
 
 /* TODO 11: Implement es_push
@@ -112,33 +204,62 @@ void es_init(EditStack *s) {
  * - Check capacity and resize if needed
  * - Add edit to array and increment size
  */
-void es_push(EditStack *s, Edit e) {
-    // TODO: Implement this function
+void es_push(EditStack *s, Edit e) 
+{
+    int newCapacity;
+    if(s->size >= s->capacity)
+    {
+        if(s->capacity == 0)
+        {
+            newCapacity = 1;
+        }
+        else
+        {
+            newCapacity = s->capacity * 2;
+        }
+
+        Edit* temp = realloc(s->edits, newCapacity*sizeof(Edit));
+        if(temp==NULL)
+        {
+            return;
+        }
+
+        s->edits = temp;
+        s->capacity = newCapacity;
+    }
+
+    s->edits[s->size] = e;
+    s->size = s->size + 1;
 }
 
 /* TODO 12: Implement es_pop
  * Similar to fs_pop but for Edit structs
  */
-Edit es_pop(EditStack *s) {
-    Edit dummy = {0};
-    // TODO: Implement this function
-    return dummy;
+Edit es_pop(EditStack *s) 
+{
+    s->size = s->size - 1;
+    return s->edits[s->size];
 }
 
 /* TODO 13: Implement es_empty
  * Return 1 if size == 0, otherwise 0
  */
-int es_empty(EditStack *s) {
-    // TODO: Implement this function
-    return 1;
+int es_empty(EditStack *s) 
+{
+    if(s->size==0)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 /* TODO 14: Implement es_clear
  * - Set size to 0 (don't free memory, just reset)
  * - This is used to clear the redo stack when a new edit is made
  */
-void es_clear(EditStack *s) {
-    // TODO: Implement this function
+void es_clear(EditStack *s) 
+{
+    s->size = 0;
 }
 
 void es_free(EditStack *s) {
@@ -158,8 +279,11 @@ void free_edit_stack(EditStack *s) {
  * - Set front and rear to NULL
  * - Set size to 0
  */
-void q_init(Queue *q) {
-    // TODO: Implement this function
+void q_init(Queue *q) 
+{
+    q->front = NULL;
+    q->rear = NULL;
+    q->size = 0;
 }
 
 /* TODO 16: Implement q_enqueue
@@ -173,8 +297,25 @@ void q_init(Queue *q) {
  *   - Update rear to point to the new node
  * - Increment size
  */
-void q_enqueue(Queue *q, Node *node, int id) {
-    // TODO: Implement this function
+void q_enqueue(Queue *q, Node *node, int id) 
+{
+    QueueNode* newQNode = (QueueNode*)malloc(sizeof(QueueNode));
+    newQNode->treeNode = node;
+    newQNode->id = id;
+    newQNode->next = NULL;
+
+    if(q->rear==NULL)
+    {
+        q->front = newQNode;
+        q->rear = newQNode;
+    }
+    else
+    {
+        q->rear->next = newQNode;
+        q->rear = newQNode;
+    }
+
+    q->size = q->size + 1;
 }
 
 /* TODO 17: Implement q_dequeue
@@ -187,25 +328,60 @@ void q_enqueue(Queue *q, Node *node, int id) {
  * - Decrement size
  * - Return 1
  */
-int q_dequeue(Queue *q, Node **node, int *id) {
-    // TODO: Implement this function
-    return 0;
+int q_dequeue(Queue *q, Node **node, int *id) 
+{
+    if(q->front==NULL)
+    {
+        return 0;
+    }
+
+    *node = q->front->treeNode;
+    *id = q->front->id;
+
+    QueueNode* temp = q->front;
+
+    q->front = q->front->next;
+
+    if(q->front==NULL)
+    {
+        q->rear = NULL;
+    }
+
+    free(temp);
+    q->size--;
+    
+    return 1;
 }
 
 /* TODO 18: Implement q_empty
  * Return 1 if size == 0, otherwise 0
  */
 int q_empty(Queue *q) {
-    // TODO: Implement this function
-    return 1;
+    if(q->size==0)
+    {
+        return 1;
+    }
+
+    return 0;
 }
 
 /* TODO 19: Implement q_free
  * - Dequeue all remaining nodes
  * - Use a loop with q_dequeue until queue is empty
  */
-void q_free(Queue *q) {
-    // TODO: Implement this function
+void q_free(Queue *q) 
+{
+    Node* n;
+    int id; 
+
+    while(q_dequeue(q, &n, &id))
+    {
+
+    }
+
+    q->front = NULL;
+    q->rear = NULL;
+    q->size = 0;
 }
 
 /* ========== Hash Table ========== */
@@ -228,9 +404,30 @@ void q_free(Queue *q) {
  * - Null-terminate result
  * - Return the new string
  */
-char *canonicalize(const char *s) {
-    // TODO: Implement this function
-    return NULL;
+char *canonicalize(const char *s) 
+{
+    char* resultBuffer = (char*)malloc(strlen(s)+1);
+    int j = 0;
+    for(int i = 0; s[i]!='\0'; i++)
+    {
+        if(isalnum(s[i]))
+        {
+            resultBuffer[j] = tolower(s[i]);
+            j++;
+        }
+        else if(isspace(s[i]))
+        {
+            resultBuffer[j] = '_';
+            j++;
+        }
+        else 
+        {
+            continue;
+        }
+    }
+
+    resultBuffer[j] = '\0';
+    return resultBuffer;
 }
 
 /* TODO 21: Implement h_hash (djb2 algorithm)
@@ -240,8 +437,12 @@ char *canonicalize(const char *s) {
  * Return hash
  */
 unsigned h_hash(const char *s) {
-    // TODO: Implement this function
-    return 0;
+    unsigned hash = 5381;
+    for(int i = 0; s[i]!='\0'; i++)
+    {
+        hash = ((hash << 5) + hash) + s[i]; 
+    }
+    return hash;
 }
 
 /* TODO 22: Implement h_init
@@ -249,8 +450,11 @@ unsigned h_hash(const char *s) {
  * - Set nbuckets field
  * - Set size to 0
  */
-void h_init(Hash *h, int nbuckets) {
-    // TODO: Implement this function
+void h_init(Hash *h, int nbuckets) 
+{
+    h->buckets = (Entry**)calloc(nbuckets, sizeof(Entry*));
+    h->nbuckets = nbuckets;
+    h->size = 0;
 }
 
 /* TODO 23: Implement h_put
@@ -271,9 +475,85 @@ void h_init(Hash *h, int nbuckets) {
  *    - Increment h->size
  *    - Return 1
  */
-int h_put(Hash *h, const char *key, int animalId) {
-    // TODO: Implement this function
-    return 0;
+int h_put(Hash *h, const char *key, int animalId) 
+{
+    int idx = h_hash(key) % h->nbuckets;
+
+    Entry* curr = h->buckets[idx];
+    while(curr!=NULL)
+    {
+        if(strcmp(curr->key, key)==0)
+        {
+            break;
+        }
+        curr = curr->next;
+    }
+
+    if(curr!=NULL)
+    {
+        for(int i = 0; i<curr->vals.count; i++)
+        {
+            if(curr->vals.ids[i]==animalId)
+            {
+                return 0;
+            }
+        }
+
+        if(curr->vals.count==curr->vals.capacity)
+        {
+            int newCap;
+            if(curr->vals.capacity>0)
+            {
+                newCap = curr->vals.capacity * 2;
+            }
+            else
+            {
+                newCap = 4;
+            }
+
+            int* newID = (int*)realloc(curr->vals.ids, newCap*sizeof(int));
+            if(newID==NULL)
+            {
+                return 0;
+            }
+            curr->vals.ids = newID;
+            curr->vals.capacity = newCap;
+        }
+
+        curr->vals.ids[curr->vals.count] = animalId;
+        curr->vals.count +=1; 
+        return 1;
+    }
+    else 
+    {
+        Entry* newEntry = (Entry*)malloc(sizeof(Entry));
+        if(newEntry==NULL)
+        {
+            return 0;   
+        }
+        newEntry->key = strdup(key);
+        if(newEntry->key==NULL)
+        {
+            free(newEntry);
+            return 0;
+        }
+        newEntry->vals.capacity = 4;
+        newEntry->vals.count = 0;
+        newEntry->vals.ids = (int*)malloc(4*sizeof(int));
+        if(newEntry->vals.ids == NULL)
+        {
+            free(newEntry->key);
+            free(newEntry);
+            return 0;
+        }
+        newEntry->vals.ids[newEntry->vals.count++] = animalId;
+
+        newEntry->next = h->buckets[idx];
+        h->buckets[idx] = newEntry;
+        h->size +=1;
+
+        return 1;
+    }
 }
 
 /* TODO 24: Implement h_contains
@@ -285,8 +565,30 @@ int h_put(Hash *h, const char *key, int animalId) {
  * 3. If found, search vals.ids array for animalId
  * 4. Return 1 if found, 0 otherwise
  */
-int h_contains(const Hash *h, const char *key, int animalId) {
-    // TODO: Implement this function
+int h_contains(const Hash *h, const char *key, int animalId) 
+{
+    int idx = h_hash(key) % h->nbuckets;
+    Entry* curr = h->buckets[idx];
+    while(curr!=NULL)
+    {
+        if(strcmp(curr->key, key)==0)
+        {
+            break;
+        }
+        curr = curr->next;
+    }
+
+    if(curr!=NULL)
+    {
+        for(int i = 0; i<curr->vals.count; i++)
+        {
+            if(curr->vals.ids[i]==animalId)
+            {
+                return 1;
+            }
+        }
+    }
+
     return 0;
 }
 
@@ -305,8 +607,20 @@ int h_contains(const Hash *h, const char *key, int animalId) {
  *    - Set *outCount = 0
  *    - Return NULL
  */
-int *h_get_ids(const Hash *h, const char *key, int *outCount) {
-    // TODO: Implement this function
+int *h_get_ids(const Hash *h, const char *key, int *outCount) 
+{
+    int idx = h_hash(key) % h->nbuckets;
+    Entry* curr = h->buckets[idx];
+    while(curr!=NULL)
+    {
+        if(strcmp(curr->key, key)==0)
+        {
+            *outCount = curr->vals.count;
+            return curr->vals.ids;
+        }
+        curr = curr->next;
+    }
+
     *outCount = 0;
     return NULL;
 }
@@ -324,6 +638,23 @@ int *h_get_ids(const Hash *h, const char *key, int *outCount) {
  * - Free the buckets array
  * - Set buckets to NULL, size to 0
  */
-void h_free(Hash *h) {
-    // TODO: Implement this function
+void h_free(Hash *h) 
+{
+    for(int i = 0; i<h->nbuckets; i++)
+    {
+        Entry* curr = h->buckets[i];
+        Entry* next;
+        while(curr!=NULL)
+        {
+            free(curr->key);
+            free(curr->vals.ids);
+            next = curr->next;
+            free(curr);
+            curr = next;
+        }
+    }
+
+    free(h->buckets);
+    h->buckets = NULL;
+    h->size = 0;
 }
--- src/game.c
+++ src/game.c
@@ -4,6 +4,7 @@
 #include <ncurses.h>
 #include "lab5.h"
 
+
 extern Node *g_root;
 extern EditStack g_undo;
 extern EditStack g_redo;
@@ -61,10 +62,143 @@ void play_game() {
     // Loop until stack empty or guess is correct
     // Handle question nodes and leaf nodes differently
     
+    //Step 2
     FrameStack stack;
     fs_init(&stack);
-    
-    // TODO: Your implementation here
+
+    //Step 3
+    fs_push(&stack, g_root, -1);
+
+    //Step 4
+    Node* parent = NULL;
+    int parentAnswer = -1;
+
+    //Step 5
+    while(stack.size!=0)
+    {
+        clear();
+        attron(COLOR_PAIR(5) | A_BOLD);
+        mvprintw(0, 0, "%-80s", " Playing 20 Questions");
+        attroff(COLOR_PAIR(5) | A_BOLD);
+        mvprintw(2, 2, "Think of an animal, and I'll try to guess it!");
+        refresh();
+
+
+        Frame currFrame = fs_pop(&stack);
+        Node* currNode = currFrame.node;
+        if(currNode->isQuestion)
+        {
+            char prompt[256];
+            snprintf(prompt, sizeof(prompt), "%s (y/n): ", currNode->text);
+            int ans = get_yes_no(5, 2, prompt);
+
+            parent = currNode;
+            parentAnswer = ans;
+
+            Node* nextNode; 
+            if(ans==1)
+            {
+                nextNode = currNode->yes;
+            }
+            else 
+            {
+                nextNode = currNode->no;
+            }
+
+            fs_push(&stack, nextNode, ans);
+        }
+        else 
+        {
+            char guessPrompt[256];
+            snprintf(guessPrompt, sizeof(guessPrompt), "Is it a %s? (y/n): ", currNode->text);
+            int correct = get_yes_no(5, 2, guessPrompt);
+
+            if(correct==1)
+            {
+                mvprintw(7, 2, "Yay! I guessed it! 🎉  Press any key...");
+                refresh();
+                getch();
+                break;
+            }
+
+            char* newAnimalName_in  = get_input(7, 2, "What animal were you thinking of? ");
+            char* newQuestionText_in= get_input(9, 2, "Give me a yes/no question to distinguish them: ");
+
+            char* newAnimalName  = strdup(newAnimalName_in);
+            char* newQuestionText= strdup(newQuestionText_in);
+
+            int newAns = get_yes_no(11, 2, "For your animal, what is the answer? (y/n): ");
+
+            Node* newQuestion = create_question_node(newQuestionText);
+            Node* newLeaf = create_animal_node(newAnimalName);
+
+            free(newAnimalName);
+            free(newQuestionText);
+
+            if(newAns==1)
+            {
+                newQuestion->yes = newLeaf;
+                newQuestion->no = currNode;
+            }
+            else 
+            {
+                newQuestion->yes = currNode;
+                newQuestion->no = newLeaf;
+            }
+
+            clear();
+            mvprintw(15, 2, "[dbg] newQ=%p  yes=%p  no=%p  oldLeaf=%p  newLeaf=%p",
+            (void*)newQuestion, (void*)newQuestion->yes, (void*)newQuestion->no,
+            (void*)currNode, (void*)newLeaf);
+            mvprintw(16, 2, "[dbg] yes txt: %s", newQuestion->yes ? newQuestion->yes->text : "<null>");
+            mvprintw(17, 2, "[dbg]  no txt: %s", newQuestion->no  ? newQuestion->no->text  : "<null>");
+            refresh(); getch();
+
+            if(parent==NULL)
+            {
+                g_root = newQuestion;
+            }
+            else 
+            {
+                if(parentAnswer==1)
+                {
+                    parent->yes = newQuestion;
+                }
+                else 
+                {
+                    parent->no = newQuestion;
+                }
+            }
+
+            Edit e;
+            e.type = EDIT_INSERT_SPLIT;
+            e.parent = parent;
+            e.wasYesChild = (parent == NULL) ? -1 : parentAnswer;
+            e.oldLeaf = currNode;
+            e.newQuestion = newQuestion;
+            e.newLeaf = newLeaf;
+            es_push(&g_undo, e);
+
+            es_clear(&g_redo);
+
+            char* canon = canonicalize(newQuestion->text);
+
+            static int nextAnimalId = 1;
+
+            if(newAns==1)
+            {
+                h_put(&g_index, canon, nextAnimalId++);
+            }
+
+            free(canon);
+
+            mvprintw(13, 2, "Thanks! I learned something new. Press any key to continue...");
+            refresh();
+            getch();
+
+            break;
+        }
+    }
     
     fs_free(&stack);
 }
@@ -87,9 +221,31 @@ void play_game() {
  * 
  * Note: We don't free newQuestion/newLeaf because they might be redone
  */
-int undo_last_edit() {
-    // TODO: Implement this function
-    return 0;
+int undo_last_edit() 
+{
+    if(g_undo.size==0)
+    {
+        return 0;
+    }
+
+    EditStack* undoPtr = &g_undo;
+    EditStack* redoPtr = &g_redo;
+    Edit edit = es_pop(undoPtr);
+
+    if(edit.parent==NULL)
+    {
+        g_root = edit.oldLeaf;
+    }
+    else if(edit.wasYesChild)
+    {
+        edit.parent->yes = edit.oldLeaf;
+    }
+    else 
+    {
+        edit.parent->no = edit.oldLeaf;
+    }
+    es_push(redoPtr, edit);
+    return 1;
 }
 
 /* TODO 33: Implement redo_last_edit
@@ -108,7 +264,31 @@ int undo_last_edit() {
  * 4. Push edit back to g_undo stack
  * 5. Return 1
  */
-int redo_last_edit() {
-    // TODO: Implement this function
-    return 0;
+int redo_last_edit() 
+{
+    if(g_redo.size==0)
+    {
+        return 0;
+    }
+
+    EditStack* redoPtr = &g_redo;
+    EditStack* undoPtr = &g_undo;
+    Edit edit = es_pop(redoPtr);
+
+    if(edit.parent==NULL)
+    {
+        g_root = edit.newQuestion;
+    }
+    else if(edit.wasYesChild)
+    {
+        edit.parent->yes = edit.newQuestion;
+    }
+    else 
+    {
+        edit.parent->no = edit.newQuestion;
+    }
+
+    es_push(undoPtr, edit);
+
+    return 1;
 }
